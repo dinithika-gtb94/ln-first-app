481665201aac1bf9966434f6b6b722d70edc54df
dinithika-gtb94 <dinithika.fg@gmail.com>
2023-04-20 12:41:14 +0530

    first commit

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,7 +5,7 @@ export default function HomePage() {
    return(
     // <div>HomePage</div>
     // <div>
-    <LoginComp name='Submitgit'/>
+      <LoginComp name='Submitgit'/>
     // </div>
    )
 }
